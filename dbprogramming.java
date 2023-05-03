8b6bdb80b5aa9dc23a657fb5c16e471bec633425
MarKKunG123 <lengtamma1310@gmail.com>
2023-05-03 10:28:40 +0700

    Update

---
+++ dbprogramming.java
@@ -0,0 +1,23 @@
+package dbprogramming;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbprogramming {
+    public static void main(String[] args) {
+        String URL = "jdbc:mysql://localhost:3306/mydb1";
+        String username = "root";
+        String password = "mysql@sit";
+        try {
+            Class.forName("JDBCDriverClass");
+            System.out.println("Driver loaded");
+            Connection connection= DriverManager.getConnection(URL, username, password);
+            System.out.println("Database Connected");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
